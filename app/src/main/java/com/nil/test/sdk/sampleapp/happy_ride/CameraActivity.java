70ec2801ca2ab745f5341ead5fbae13becdbb4de
itan <itanbp@gmail.com>
2018-06-05 21:45:34 +0300

    camera working

--- app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/CameraActivity.java
+++ app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/CameraActivity.java
@@ -1,16 +1,36 @@
 package com.nil.test.sdk.sampleapp.happy_ride;
 
+import android.Manifest;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.nil.test.sdk.sampleapp.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import ai.deepar.ar.AREventListener;
 import ai.deepar.ar.CameraGrabber;
+import ai.deepar.ar.CameraGrabberListener;
+import ai.deepar.ar.CameraOrientation;
+import ai.deepar.ar.CameraResolutionPreset;
 import ai.deepar.ar.DeepAR;
 
-public class CameraActivity extends PermissionsActivity {
+public class CameraActivity extends PermissionsActivity implements AREventListener, SurfaceHolder.Callback {
 
 
     private SurfaceView arView;
@@ -19,11 +39,31 @@ public class CameraActivity extends PermissionsActivity {
 
     private CameraGrabber cameraGrabber;
     private DeepAR deepAR;
+    private int screenOrientation;
+    ArrayList<AREffect> masks;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        screenOrientation = getScreenOrientation();
+        deepAR = new DeepAR(this);
+
+        switch (screenOrientation) {
+            case ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE:
+                deepAR.setCameraOrientation(CameraOrientation.LANDSCAPE_LEFT);
+                break;
+            case ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE:
+                deepAR.setCameraOrientation(CameraOrientation.LANDSCAPE_RIGHT);
+                break;
+            default:
+                deepAR.setCameraOrientation(CameraOrientation.PORTRAIT);
+                break;
+        }
+
+        deepAR.setAntialiasingLevel(4);
+        deepAR.initialize(this, this, CameraResolutionPreset.P640x480);
     }
 
 
@@ -50,6 +90,220 @@ public class CameraActivity extends PermissionsActivity {
         return "";
     }
 
+
+    private int getScreenOrientation() {
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        int orientation;
+        // if the device's natural orientation is portrait:
+        if ((rotation == Surface.ROTATION_0
+                || rotation == Surface.ROTATION_180) && height > width ||
+                (rotation == Surface.ROTATION_90
+                        || rotation == Surface.ROTATION_270) && width > height) {
+            switch (rotation) {
+                case Surface.ROTATION_0:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                    break;
+                case Surface.ROTATION_90:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                    break;
+                case Surface.ROTATION_180:
+                    orientation =
+                            ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+                    break;
+                case Surface.ROTATION_270:
+                    orientation =
+                            ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+                    break;
+                default:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                    break;
+            }
+        }
+        // if the device's natural orientation is landscape or if the device
+        // is square:
+        else {
+            switch (rotation) {
+                case Surface.ROTATION_0:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                    break;
+                case Surface.ROTATION_90:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                    break;
+                case Surface.ROTATION_180:
+                    orientation =
+                            ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+                    break;
+                case Surface.ROTATION_270:
+                    orientation =
+                            ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+                    break;
+                default:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                    break;
+            }
+        }
+
+        return orientation;
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        String cameraPermission = getResources().getString(R.string.camera_permission);
+        String externalStoragePermission = getResources().getString(R.string.external_permission);
+
+        checkMultiplePermissions(
+                Arrays.asList(Manifest.permission.CAMERA),
+                cameraPermission + " " + externalStoragePermission,
+                100,
+                new PermissionsActivity.MultiplePermissionsCallback() {
+                    @Override
+                    public void onAllPermissionsGranted() {
+                        setupViews();
+                    }
+
+                    @Override
+                    public void onPermissionsDenied(List<String> deniedPermissions) {
+                        Log.d("MainActity", "Permissions Denied!");
+                    }
+                });
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        cameraGrabber.setFrameReceiver(null);
+        cameraGrabber.stopPreview();
+        cameraGrabber.releaseCamera();
+        cameraGrabber = null;
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        deepAR.release();
+    }
+
+
+    private void setupEffects() {
+
+        masks = new ArrayList<>();
+        masks.add(new AREffect("none", AREffect.EffectTypeMask));
+        masks.add(new AREffect("kanye", AREffect.EffectTypeMask));
+
+    }
+
+
+    private void setupViews() {
+
+        arView = (SurfaceView) findViewById(R.id.surface);
+
+        arView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deepAR.onClick();
+            }
+        });
+
+        arView.getHolder().addCallback(this);
+
+        // Surface might already be initialized, so we force the call to onSurfaceChanged
+        arView.setVisibility(View.GONE);
+        arView.setVisibility(View.VISIBLE);
+
+
+        cameraGrabber = new CameraGrabber();
+
+        switch (screenOrientation) {
+            case ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE:
+                cameraGrabber.setScreenOrientation(90);
+                break;
+            case ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE:
+                cameraGrabber.setScreenOrientation(270);
+                break;
+            default:
+                cameraGrabber.setScreenOrientation(0);
+                break;
+        }
+
+        cameraGrabber.setResolutionPreset(CameraResolutionPreset.P640x480);
+        cameraGrabber.initCamera(new CameraGrabberListener() {
+            @Override
+            public void onCameraInitialized() {
+                cameraGrabber.setFrameReceiver(deepAR);
+                cameraGrabber.startPreview();
+            }
+
+            @Override
+            public void onCameraError(String errorMsg) {
+            }
+        });
+
+        setupEffects();
+
+        captureButton = (ImageView) findViewById(R.id.camera_capture_button);
+        captureButton.setOnClickListener(v -> deepAR.takeScreenshot());
+
+    }
+
+    @Override
+    public void screenshotTaken(final Bitmap screenshot) {
+        if (screenshot != null) {
+        }
+    }
+
+
+    @Override
+    public void videoRecordingStarted() {
+    }
+
+    @Override
+    public void videoRecordingFinished() {
+    }
+
+    @Override
+    public void videoRecordingFailed() {
+    }
+
+    @Override
+    public void initialized() {
+
+    }
+
+    @Override
+    public void faceVisibilityChanged(boolean faceVisible) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+        deepAR.setRenderSurface(surfaceHolder.getSurface(), width, height);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        deepAR.setRenderSurface(null, 0, 0);
+    }
+
+    @Override
+    public void videoRecordingPrepared() {
+    }
+
+    @Override
+    public void error(String error) {
+    }
 
 
 }
