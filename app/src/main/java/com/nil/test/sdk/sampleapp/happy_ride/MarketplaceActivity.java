f3502c78aa20bcd1cb021dfe3946f25d5a42d4c9
itan <itanbp@gmail.com>
2018-06-05 16:26:35 +0300

    complete ride request flow

--- app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/MarketplaceActivity.java
+++ app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/MarketplaceActivity.java
@@ -5,20 +5,36 @@ import android.support.annotation.NonNull;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.google.common.collect.Lists;
+import com.here.mobility.sdk.core.net.ResponseException;
+import com.here.mobility.sdk.core.net.ResponseFuture;
+import com.here.mobility.sdk.core.net.ResponseListener;
+import com.here.mobility.sdk.demand.CreateRideRequest;
+import com.here.mobility.sdk.demand.DemandClient;
+import com.here.mobility.sdk.demand.PassengerDetails;
 import com.here.mobility.sdk.demand.PublicTransportRideOffer;
+import com.here.mobility.sdk.demand.Ride;
 import com.here.mobility.sdk.demand.RideOffer;
 import com.here.mobility.sdk.demand.TaxiRideOffer;
 import com.nil.test.sdk.sampleapp.R;
+import com.nil.test.sdk.sampleapp.ride_offers.RideOffersActivity;
+import com.nil.test.sdk.sampleapp.ride_status.RideStatusActivity;
 
 import java.util.ArrayList;
 
+import static com.nil.test.sdk.sampleapp.happy_ride.OrderConfirmationActivity.CONFIRMATION_KEY;
 import static com.nil.test.sdk.sampleapp.ride_offers.RideOffersActivity.EXTRA_PT_RIDE_OFFER_LIST;
 import static com.nil.test.sdk.sampleapp.ride_offers.RideOffersActivity.EXTRA_TAXI_RIDE_OFFER_LIST;
 
 public class MarketplaceActivity extends BaseActivity {
 
+
+    /**
+     * Use DemandClient to request ride.
+     */
+    private DemandClient demandClient;
 
     private RecyclerView.Adapter offersAdapter;
     private RecyclerView offersRecycler;
@@ -28,6 +44,7 @@ public class MarketplaceActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        demandClient = DemandClient.newInstance(this);
         offersRecycler = (RecyclerView) findViewById(R.id.marketplace_offers_recycler);
 
         initViews();
@@ -56,6 +73,34 @@ public class MarketplaceActivity extends BaseActivity {
         return getString(R.string.market_place_title);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //It's important to call shutdown function when the client is no longer needed.
+        if (demandClient != null) {
+            demandClient.shutdownNow();
+        }
+    }
+
+
+    /**
+     * Future ride listener.
+     */
+    private ResponseListener<Ride> rideFutureListener = new ResponseListener<Ride>() {
+        @Override
+        public void onResponse(Ride ride) {
+            Intent intent = new Intent(MarketplaceActivity.this, OrderConfirmationActivity.class);
+            intent.putExtra(CONFIRMATION_KEY, ride.getRideId());
+            startActivity(intent);
+        }
+
+        @Override
+        public void onError(@NonNull ResponseException e) {
+        }
+    };
+
+
     private void initViews() {
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
@@ -71,8 +116,7 @@ public class MarketplaceActivity extends BaseActivity {
             offer.accept(new RideOffer.Visitor<Void>() {
                 @Override
                 public Void visit(@NonNull TaxiRideOffer taxiRideOffer) {
-                    Intent intent = new Intent(MarketplaceActivity.this, OrderConfirmationActivity.class);
-                    startActivity(intent);
+                    requestRide(taxiRideOffer);
                     return null;
                 }
 
@@ -87,7 +131,25 @@ public class MarketplaceActivity extends BaseActivity {
     }
 
 
-    private void handleOfferSelection() {
+    /**
+     * Request to book Ride Offer.
+     *
+     * @param taxiRideOffer An offer for ride. should be received from RideOffersRequest.
+     */
+    private void requestRide(@NonNull TaxiRideOffer taxiRideOffer) {
+
+        PassengerDetails passengerDetails = PassengerDetails.builder()
+                .setName("Lior")
+                .setPhoneNumber("054")
+                .build();
+
+        CreateRideRequest rideRequest = CreateRideRequest.create(taxiRideOffer.getOfferId(), passengerDetails);
+
+        //Request to book a ride.
+        ResponseFuture<Ride> rideRequestFuture = demandClient.createRide(rideRequest);
+
+        //Register for ride request updates.
+        rideRequestFuture.registerListener(rideFutureListener);
 
     }
 
--- app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/OrderConfirmationActivity.java
+++ app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/OrderConfirmationActivity.java
@@ -2,14 +2,34 @@ package com.nil.test.sdk.sampleapp.happy_ride;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.nil.test.sdk.sampleapp.R;
 
+import static com.nil.test.sdk.sampleapp.happy_ride.HomeActivity.CONCERT_KEY;
+
 public class OrderConfirmationActivity extends BaseActivity {
+
+    public static final String CONFIRMATION_KEY = "CONFIRMATION_KEY";
+
+
+    private String confirmationNumber;
+    private TextView orderNumber;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        orderNumber = (TextView) findViewById(R.id.confirmation_order_number);
+
+        if (getIntent().hasExtra(CONFIRMATION_KEY)) {
+            confirmationNumber = getIntent().getStringExtra(CONFIRMATION_KEY);
+            String text = String.format(getString(R.string.confirmation_order_number), confirmationNumber);
+            orderNumber.setText(text);
+        }
 
     }
 
