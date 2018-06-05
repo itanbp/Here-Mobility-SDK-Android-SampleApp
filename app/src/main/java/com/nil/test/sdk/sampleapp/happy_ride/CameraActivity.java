106a2b86759859da2b7cb0219e87d017489cc573
itan <itanbp@gmail.com>
2018-06-05 21:00:44 +0300

    add augmented reality dependency

--- app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/CameraActivity.java
+++ app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/CameraActivity.java
@@ -2,12 +2,54 @@ package com.nil.test.sdk.sampleapp.happy_ride;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.ImageView;
 
-public class CameraActivity extends AppCompatActivity {
+import com.nil.test.sdk.sampleapp.R;
+
+import ai.deepar.ar.CameraGrabber;
+import ai.deepar.ar.DeepAR;
+
+public class CameraActivity extends PermissionsActivity {
+
+
+    private SurfaceView arView;
+    private ImageView captureButton;
+    private ImageView switchMaskButton;
+
+    private CameraGrabber cameraGrabber;
+    private DeepAR deepAR;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
     }
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_camera;
+    }
+
+
+    @Override
+    protected int getStatusBarColor() {
+        return R.color.material_grey_400;
+    }
+
+
+    @Override
+    protected boolean showToolbar() {
+        return true;
+    }
+
+
+    @Override
+    protected String getToolbarTitle() {
+        return "";
+    }
+
+
+
 }
--- app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/OrderConfirmationActivity.java
+++ app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/OrderConfirmationActivity.java
@@ -1,7 +1,10 @@
 package com.nil.test.sdk.sampleapp.happy_ride;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nil.test.sdk.sampleapp.R;
@@ -15,6 +18,7 @@ public class OrderConfirmationActivity extends BaseActivity {
 
     private String confirmationNumber;
     private TextView orderNumber;
+    private ImageView cameraImage;
 
 
 
@@ -24,12 +28,18 @@ public class OrderConfirmationActivity extends BaseActivity {
 
 
         orderNumber = (TextView) findViewById(R.id.confirmation_order_number);
+        cameraImage = (ImageView) findViewById(R.id.confirmation_camera_image);
 
         if (getIntent().hasExtra(CONFIRMATION_KEY)) {
             confirmationNumber = getIntent().getStringExtra(CONFIRMATION_KEY);
             String text = String.format(getString(R.string.confirmation_order_number), confirmationNumber);
             orderNumber.setText(text);
         }
+
+        cameraImage.setOnClickListener(v -> {
+            Intent intent = new Intent(this, CameraActivity.class);
+            startActivity(intent);
+        });
 
     }
 
