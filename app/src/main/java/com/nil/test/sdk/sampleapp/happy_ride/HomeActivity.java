38133b47e6c503ef2383ae31808496b28b2cc0e2
itan <itanbp@gmail.com>
2018-06-05 22:38:38 +0300

    fix bug - missing register user logic

--- app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/HomeActivity.java
+++ app/src/main/java/com/nil/test/sdk/sampleapp/happy_ride/HomeActivity.java
@@ -9,14 +9,23 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.here.mobility.sdk.common.util.PermissionUtils;
+import com.here.mobility.sdk.core.auth.UserAuthenticationException;
+import com.here.mobility.sdk.core.net.ResponseException;
+import com.here.mobility.sdk.core.net.ResponseListener;
+import com.here.mobility.sdk.demand.DemandClient;
+import com.here.mobility.sdk.demand.RideQuery;
+import com.here.mobility.sdk.demand.RideQueryResponse;
 import com.here.mobility.sdk.map.FusedUserLocationSource;
 import com.here.mobility.sdk.map.MapController;
 import com.here.mobility.sdk.map.MapFragment;
 import com.here.mobility.sdk.map.MapView;
 import com.nil.test.sdk.sampleapp.R;
 import com.nil.test.sdk.sampleapp.get_rides.GetRidesActivity;
+import com.nil.test.sdk.sampleapp.registration.RegistrationDialog;
+import com.nil.test.sdk.sampleapp.util.AuthUtils;
 import com.nil.test.sdk.sampleapp.util.Constant;
 
 
@@ -44,6 +53,11 @@ public class HomeActivity extends BaseActivity implements MapView.MapReadyListen
      */
     private MapController mapController;
 
+    /**
+     * Use DemandClient to request ride offers.
+     */
+    private DemandClient demandClient;
+
 
     private RecyclerView.Adapter galleryAdapter;
     private RecyclerView galleryRecycler;
@@ -54,6 +68,9 @@ public class HomeActivity extends BaseActivity implements MapView.MapReadyListen
         super.onCreate(savedInstanceState);
 
         galleryRecycler = (RecyclerView) findViewById(R.id.home_concerts_gallery);
+
+        //Initialize DemandClient.
+        demandClient = DemandClient.newInstance(this);
 
         initViews();
     }
@@ -87,9 +104,9 @@ public class HomeActivity extends BaseActivity implements MapView.MapReadyListen
     }
 
 
-
     /**
      * this callback is called when the map is set-up, before we render any tiles to the screen - so this is the place to set those values
+     *
      * @param mapController map controller to interact with the map.
      */
     @Override
@@ -109,6 +126,73 @@ public class HomeActivity extends BaseActivity implements MapView.MapReadyListen
     }
 
 
+    /**
+     * Get rides response listener.
+     */
+    @NonNull
+    private ResponseListener<RideQueryResponse> getRideListener = new ResponseListener<RideQueryResponse>() {
+
+        @Override
+        public void onResponse(@NonNull RideQueryResponse rideQueryResponse) {
+            //setActiveRides(rideQueryResponse.getRides());
+        }
+
+        @Override
+        public void onError(@NonNull ResponseException e) {
+            if(e.getCause().getCause() instanceof UserAuthenticationException){
+                showRegistrationDialog();
+            }else{
+                Toast.makeText(HomeActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Get Active rides.
+        getActiveRides();
+    }
+
+
+    /**
+     * Show registration dialog.
+     */
+    private void showRegistrationDialog() {
+        RegistrationDialog dialog = new RegistrationDialog(HomeActivity.this);
+        dialog.setPositiveButton(R.string.register, (d, which) -> {
+            String userName = dialog.getUserName();
+            if (!userName.isEmpty()) {
+
+                // The user registration should be done with your app's backend (see the documentation for more info).
+                // This is a snippet to generate the token in the app, for testing purposes.
+                AuthUtils.registerUser(userName,
+                        getString(R.string.here_sdk_app_id),
+                        getString(R.string.here_sdk_app_secret));
+                //getActiveRides();
+            } else {
+                Toast.makeText(this, R.string.register_not_valid_user_name, Toast.LENGTH_LONG).show();
+            }
+        });
+        dialog.show();
+    }
+
+    /**
+     * Request ride attach to user.
+     */
+    private void getActiveRides() {
+
+        //Build a ride query.
+        RideQuery rideQuery = RideQuery.builder()
+                .setStatusFilter(RideQuery.StatusFilter.ALL)
+                .build();
+
+        //get the rides and register a listener.
+        demandClient.getRides(rideQuery).registerListener(getRideListener);
+    }
+
     @Override
     public void onMapFailure(@NonNull Exception e) {
         Log.e(LOG_TAG, "onMapFailure: ", e);
@@ -116,9 +200,9 @@ public class HomeActivity extends BaseActivity implements MapView.MapReadyListen
 
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-        if (requestCode == LOCATION_PERMISSIONS_CODE){
-            if ((grantResults.length > 0) && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSIONS_CODE) {
+            if ((grantResults.length > 0) && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 startLocationUpdates();
             }
         }
@@ -135,11 +219,22 @@ public class HomeActivity extends BaseActivity implements MapView.MapReadyListen
      * Start user location updates.
      */
     @SuppressLint("MissingPermission")
-    private void startLocationUpdates(){
+    private void startLocationUpdates() {
 
         mapController.getUserLocationMarkerManager().setLocationSource(new FusedUserLocationSource(this));
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //It's important to call shutdownNow function when the client is no longer needed.
+        if (demandClient != null) {
+            demandClient.shutdownNow();
+        }
+    }
+
 
 
 }
